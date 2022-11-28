c3f64ac02d94742fa1c27dbdbef5d3e200c31a38
clicknetcafe <rama4shared@gmail.com>
2022-11-28 14:40:24 +0800

    avoid duplicates

--- plugins/group-creator.js
+++ plugins/group-creator.js
@@ -1,10 +1,10 @@
-function handler(m) {
-  const data = [...global.owner.filter(([id, isCreator]) => id && isCreator), ...global.db.data.owner.filter(([id, isCreator]) => id && isCreator)]
-  this.sendContact(m.chat, data.map(([id, name]) => [id, name]), m)
+let handler = async (m) => {
+	const data = [...global.owner.filter(([id, isCreator]) => id && isCreator), ...global.db.data.owner.filter(([id, isCreator]) => id && isCreator)]
+	await conn.sendContact(m.chat, data.map(([id, name]) => [id, name]), m)
 }
-handler.menugroup = ['owner', 'creator']
-handler.tagsgroup = ['group']
 
+handler.menugroup = ['owner']
+handler.tagsgroup = ['group']
 handler.command = /^(owner|creator)$/i
 
 export default handler
