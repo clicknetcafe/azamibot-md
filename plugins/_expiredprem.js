0d3e920186262a7e7f23824ec1b5beaedd1c965c
clicknetcafe <rama4shared@gmail.com>
2022-12-28 02:43:11 +0800

    improve broadcast and mute

--- plugins/_expiredprem.js
+++ plugins/_expiredprem.js
@@ -1,11 +1,15 @@
 export async function all(m) {
-    let chats = global.db.data.users[m.sender]
-    let prems = global.db.data.datas.prems
-    if (!chats.expired)
-        return !0
-    if (+new Date() > chats.expired) {
-        await this.sendMessage(m.chat, { text: '[!] Durasi premium anda telah berakhir.', mentions: [m.sender] }, { quoted: m })
-        chats.expired = null
-        global.db.data.datas.prems = await prems.filter(v => v.user !== m.sender)
-    }
+	let chats = global.db.data.users[m.sender]
+	if (!chats.expired)
+		return !0
+	if (+new Date() > chats.expired) {
+		try {
+			let prems = global.db.data.datas.prems
+			await this.sendMessage(m.chat, { text: '[!] Durasi premium anda telah berakhir.', mentions: [m.sender] }, { quoted: m })
+			chats.expired = null
+			global.db.data.datas.prems = await prems.filter(v => v.user !== m.sender)
+		} catch (e) {
+			console.log(e)
+		}
+	}
 }
