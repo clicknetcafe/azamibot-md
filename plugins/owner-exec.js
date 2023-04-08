e47bc6581362b21ea3e508b3a8e6d72d30f2389d
clicknetcafe <rama4shared@gmail.com>
2023-04-08 23:19:45 +0800

    fs added to exec

--- plugins/owner-exec.js
+++ plugins/owner-exec.js
@@ -5,6 +5,7 @@ import { fileURLToPath } from 'url'
 import { dirname } from 'path'
 import { createRequire } from 'module'
 import fetch from 'node-fetch'
+import fs from 'fs'
 
 const __dirname = dirname(fileURLToPath(import.meta.url))
 const require = createRequire(__dirname)
@@ -20,12 +21,12 @@ let handler = async (m, _2) => {
 		let f = {
 			exports: {}
 		}
-		let exec = new (async () => { }).constructor('print', 'm', 'handler', 'require', 'conn', 'db', 'fetch', 'Array', 'process', 'args', 'groupMetadata', 'module', 'exports', 'argument', _text)
+		let exec = new (async () => { }).constructor('print', 'm', 'handler', 'require', 'conn', 'db', 'fs', 'fetch', 'Array', 'process', 'args', 'groupMetadata', 'module', 'exports', 'argument', _text)
 		_return = await exec.call(conn, (...args) => {
 			if (--i < 1) return
 			console.log(...args)
 			return conn.reply(m.chat, format(...args), m)
-		}, m, handler, require, conn, db, fetch, CustomArray, process, args, groupMetadata, f, f.exports, [conn, _2])
+		}, m, handler, require, conn, db, fs, fetch, CustomArray, process, args, groupMetadata, f, f.exports, [conn, _2])
 	} catch (e) {
 		let err = syntaxerror(_text, 'Execution Function', {
 			allowReturnOutsideFunction: true,
